a8e7672186a79bb0e704a7e0a010044a99b16b30
AquaFantom <herobrine200403@gmail.com>
2025-06-19 13:39:18 +0300

    added update_info

--- src/cam_recognition.py
+++ src/cam_recognition.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 
-class Face_recognition:
+class CamRecognition:
     def __init__(self, video_capture, known_face_encodings, known_face_names):
         self.video_capture = video_capture
         self.known_face_encodings = known_face_encodings
@@ -14,11 +14,15 @@ class Face_recognition:
         self.process_this_frame = True
 
 
+    def update_info(self, new_face_encodings, new_face_names):
+        self.known_face_encodings = new_face_encodings
+        self.known_face_names = new_face_names
+
+
     def main_cycle(self):
         while True:
             ret, frame = self.video_capture.read()
 
-            # Only process every other frame of video to save time
             if self.process_this_frame:
                 # Resize frame of video to 1/4 size for faster face recognition processing
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -26,7 +30,6 @@ class Face_recognition:
                 # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
                 rgb_small_frame = small_frame[:, :, ::-1]
 
-                # Find all the faces and face encodings in the current frame of video
                 face_locations = face_recognition.face_locations(rgb_small_frame)
                 face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
@@ -36,7 +39,7 @@ class Face_recognition:
                     matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
                     name = "Unknown"
 
-                    # use the known face with the smallest distance to the new face
+                    # Use the known face with the smallest distance to the new face
                     face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
                     best_match_index = np.argmin(face_distances)
                     if matches[best_match_index]:
